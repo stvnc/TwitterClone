c75bd4065520cf2cc938684a6a473072c20f9a8e
stvnc <vincentangelo96@yahoo.com>
2020-05-17 22:11:05 +0700

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Vincent Angelo on 17/05/20.
+//  Copyright © 2020 Vincent Angelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
